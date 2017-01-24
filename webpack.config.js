0d45ed036d83744381f4c2b794eb35a2efca2d5c
Ken Lee <kzl5010@gmail.com>
2017-01-24 12:11:07 -0800

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: './',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
